e638d3cc95ded8fa80fa0f7c79e3c30e83efffd9
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-01-19 19:51:59 -0300

    interação com as tarefas

--- script-crud.js
+++ script-crud.js
@@ -5,8 +5,10 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancela = document.querySelector('.app__form-footer__button--cancel')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas'))  || []
+let tarefaSelecionada = null
 
 const limparFormulario = () => {
     textarea.value = ''
@@ -42,8 +44,9 @@ function criarElementoTarefa (tarefa) {
     botao.classList.add('app__button-edit')
 
     botao.onclick = () => {
+        //debugger
         const novaDescricao = prompt("Qual é o novo nome da tarefa?")
-        console.log('Nova descrição da tarefa: ', novaDescricao)
+        //console.log('Nova descrição da tarefa: ', novaDescricao)
         if (novaDescricao) {
             paragrafo.textContent = novaDescricao 
             tarefa.descricao = novaDescricao
@@ -60,6 +63,23 @@ function criarElementoTarefa (tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => { 
+        document.querySelectorAll('.app__section-task-list-item-active')
+          .forEach(elemento => {
+              elemento.classList.remove('app__section-task-list-item-active')
+          })
+          
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''    
+            tarefaSelecionada = null   
+            return     
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+       
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
